2dc0449cf680c311bbc8e0507023774be85a1815
“Shubham” <shubhamjain.acts@gmail.com>
2020-03-22 17:52:48 +0530

    Monthly subscription class

--- sms/src/main/java/sms/MonthlySubcription.java
+++ sms/src/main/java/sms/MonthlySubcription.java
@@ -1,7 +1,9 @@
 package sms;
 
+//Daily subscription class
 public class MonthlySubcription extends SubscriptionCommons {
 
+	// Method to calculate Monthly Subscription
 	public void calculateMonthlySubcription(String subcriptionType, String[] items) {
 		calculateBy(subcriptionType, items);
 	}
