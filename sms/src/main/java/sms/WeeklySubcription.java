61a4d0116df50edf2dee0c58020083416ef6e12e
“Shubham” <shubhamjain.acts@gmail.com>
2020-03-22 17:51:56 +0530

    Weelky Subscription class

--- sms/src/main/java/sms/WeeklySubcription.java
+++ sms/src/main/java/sms/WeeklySubcription.java
@@ -1,7 +1,9 @@
 package sms;
 
+//Weekly subscription class
 public class WeeklySubcription extends SubscriptionCommons {
 
+	// Method to calculate Weekly Subscription
 	public void calculateWeeklySubcription(String subcriptionType, String[] items) {
 		calculateBy(subcriptionType, items);
 	}
