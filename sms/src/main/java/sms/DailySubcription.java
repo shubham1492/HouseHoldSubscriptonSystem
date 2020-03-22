9c3ad1ed2cdb24b14d16b28bf90371a1440abd7c
“Shubham” <shubhamjain.acts@gmail.com>
2020-03-22 17:51:37 +0530

    Daily Subscription class

--- sms/src/main/java/sms/DailySubcription.java
+++ sms/src/main/java/sms/DailySubcription.java
@@ -1,7 +1,9 @@
 package sms;
 
+//Daily subscription class
 public class DailySubcription extends SubscriptionCommons {
 
+	// Method to calculate Daily Subscription
 	public void calculateDailySubcription(String subcriptionType, String[] items) {
 		calculateBy(subcriptionType, items);
 		/*
