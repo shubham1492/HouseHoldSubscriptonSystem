3b6bfda56acebef286ae18bd8f6b7686fa83033a
“Shubham” <shubhamjain.acts@gmail.com>
2020-03-22 18:21:38 +0530

    Core class to handle other subscription class oject

--- sms/src/main/java/sms/SubcriptionCore.java
+++ sms/src/main/java/sms/SubcriptionCore.java
@@ -2,6 +2,7 @@ package sms;
 
 import java.util.Calendar;
 
+//Core class it will provide all subscription class object
 public class SubcriptionCore extends ExcelManager {
 
 	protected static DailySubcription dailySubcription;
